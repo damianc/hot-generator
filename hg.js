09f15307fd2d247be5dd13c19b8b6fba387b9fa0
damianc <damianczapiewski@gmail.com>
2019-07-17 20:45:07 +0200

    Add `last()` method to HotGenerator

--- hg.js
+++ hg.js
@@ -1,12 +1,14 @@
 class HotGenerator {
     constructor(generator) {
         var gen = generator();
+        var last;
         
         return function () {
             return {
-                next: (v) => gen.next(v),
+                next: (v) => last = gen.next(v),
                 return: (fv) => gen.return(fv),
-                throw: (e) => gen.throw(e)
+                throw: (e) => gen.throw(e),
+                last: () => last
             };
         };
     }
