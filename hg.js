d0b3aba7905e632e561d50fff6b469334f2a9053
damianc <damianczapiewski@gmail.com>
2019-07-18 19:27:03 +0200

    Move methods to the returned function from single instances of the generator

--- hg.js
+++ hg.js
@@ -3,16 +3,23 @@ class HotGenerator {
         var gen = generator();
         var last;
         
-        return function () {
+        function hotGenFn() {
             var hotGenAPI = {
                 next: (v) => last = gen.next(v),
                 return: (fv) => gen.return(fv),
-                throw: (e) => gen.throw(e),
-                last: () => last
+                throw: (e) => gen.throw(e)
             };
 
             Object.setPrototypeOf(hotGenAPI, gen);
             return hotGenAPI;
         };
+
+        Object.defineProperty(hotGenFn, 'last', {
+            configurable: false,
+            enumerable: true,
+            get: () => () => last
+        });
+
+        return hotGenFn;
     }
 }
